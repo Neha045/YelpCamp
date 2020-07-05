8aa1f81dafaa84f0bed15a6569cfc99cf0467d68
Neha Sonpure <sonpureneha@gmail.com>
2020-07-05 16:44:03 +0000

    Add search

--- models/users.js
+++ models/users.js
@@ -1,14 +1,16 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
-	username : String,
+	username: {type: String, unique: true, required: true},
 	password : String,
 	 isAdmin: {type: Boolean, default: false},
 	avatar: String,
 	firstname: String,
 	lastname: String,
 	email: String,
-	email: {type: String, unique: true, required: true},
+	 email: {type: String, unique: true, required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     resetPasswordToken: String,
     resetPasswordExpires: Date,
 });
